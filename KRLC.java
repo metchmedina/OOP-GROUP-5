7b109961b7f41d5c08c5036da647e0e0c4a4fdb0
metchmedina <117733082+metchmedina@users.noreply.github.com>
2023-12-13 09:51:09 +0800

    Update KRLC.java

--- KRLC.java
+++ KRLC.java
@@ -1,4 +1,4 @@
-import java.awt.Font;
+import java.awt.*;
 
 import javax.swing.*;
 
@@ -7,8 +7,27 @@ public class KRLC {
 	private static	JPanel panel = new JPanel();
 	private static	JLabel label;
 	private static 	JLabel label1;
-
+	private static 	JComboBox<String> comboBox;
 	
+	
+	String[] locations = {"Adlaon", "Agsungot", "Apas", "Babag", "Bacayan", "Banilad",
+                		  "Basak Pardo", "Basak San Nicolas", "Binaliw", "Bonbon",
+                		  "Budla-an", "Buhisan", "Bulacao", "Buot-Taup Pardo", "Busay",
+                		  "Calamba", "Cambinocot", "Camputhaw", "Capitol Site", "Carreta",
+                		  "Central", "Cogon Pardo", "Cogon Ramos", "Day-as", "Duljo", 
+                		  "Ermita", "Guadalupe", "Guba", "Hippodromo", "Inayawan",
+                		  "Kalubihan", "Kalunasan", "Kamagayan", "Kasambagan", "Kinasang-an Pardo",
+                		  "Labangon", "Lahug", "Lorega", "Lusaran", "Luz", "Mabini", "Mabolo",
+                		  "Malubog", "Mambaling", "Pahina Central", "Pahina San Nicolas",
+                		  "Pamutan", "Pardo", "Pari-an", "Paril", "Pasil", "Pit-os",
+                		  "Pulangbato", "Pung-ol-Sibunga", "Punta Princesa", "Quiot Pardo",
+                		  "Sambag I", "Sambag II", "San Antonio", "San Jose",
+                		  "San Nicolas Central", "San Roque(Ciudad)", "Santa Cruz",
+                		  "Sapangdaku", "Sawang Calero", "Sinsin", "Sirao", "Suba Poblacion",
+                		  "Sudlon I", "Sudlon II", "Tabunan", "Tagbao", "Talamban", "Taptap",
+                		  "Tejero", "Tinago", "Tisa", "To-ong Pardo", "T. Padilla", "Zapatera"};
+	
+		
 	KRLC() {
 		frame.setSize(750, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,6 +46,18 @@ public class KRLC {
 		label1.setFont(new Font(null,Font.PLAIN,17));
 		panel.add(label1);
 		
+		comboBox = new JComboBox<>(locations);
+		comboBox.setFont(new Font(null,Font.PLAIN,15));
+		comboBox.setBounds(90, 60, 90, 30);
+		panel.add(comboBox);
+		
 		frame.setVisible(true);
 	}
+	
+
+		
+	
+	
+	
+	
 }
